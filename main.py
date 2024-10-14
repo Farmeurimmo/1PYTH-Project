908551be9817ea624c6bb99903017ab3ab9d68f2
Farmeurimmo <contact@farmeurimmo.fr>
2024-10-14 08:30:19 +0200

    Added function possible_square

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def new_board(n):
 def display_board(board, n):
     max_length = 0
     for i in range(n):
-        text = str(i+1) + (len(str(n)) - len(str(i+1))) * " " + " | "
+        text = str(i + 1) + (len(str(n)) - len(str(i + 1))) * " " + " | "
         for j in range(n):
             text += ("." if board[i][j] == 0 else ("x" if board[i][j] == 1 else "o")) + " " * len(str(n))
         print(text)
@@ -20,9 +20,20 @@ def display_board(board, n):
     print(len(str(n)) * " " + 3 * " ", end="")
 
     for i in range(n):
-        print(i+1, end=" " + " " * (len(str(n)) - len(str(i+1))))
+        print(i + 1, end=" " + " " * (len(str(n)) - len(str(i + 1))))
 
 
+def possible_square(board, n, player, i, j):
+    if i - 1 > 0 and board[i - 1][j] != player:
+        return False
+    if i + 1 < n and board[i + 1][j] != player:
+        return False
+    if j - 1 > 0 and board[i][j - 1] != player:
+        return False
+    if j + 1 < n and board[i][j + 1] != player:
+        return False
+    if board[i][j]:
+        return False
 
 
 n = 120
